f593d55eeffe5a22869e17edf431b47a55ee233c
Anton <2hammer90@mail.ru>
2016-02-28 13:58:50 +0300

    enable application menu

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
 var ipc = require("electron").ipcMain;
+var Menu = require("menu");
 
 // prevent window being garbage collected
 let mainWindow;
@@ -26,6 +27,37 @@ function createMainWindow() {
     return win;
 }
 
+function setApplicationMenu() {
+    var template = [
+        {
+            label: "Application",
+            submenu: [
+                {label: "About Application", selector: "orderFrontStandardAboutPanel:"},
+                {type: "separator"},
+                {
+                    label: "Quit", accelerator: "Command+Q", click: function () {
+                    app.quit();
+                }
+                }
+            ]
+        },
+        {
+            label: "Edit",
+            submenu: [
+                {label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:"},
+                {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:"},
+                {type: "separator"},
+                {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+                {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+                {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"},
+                {label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:"}
+            ]
+        }
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
 ipc.on('close-main-window', () => {
     mainWindow.close();
 });
@@ -44,6 +76,7 @@ app.on('activate', () => {
 
 app.on('ready', () => {
     mainWindow = createMainWindow();
+    setApplicationMenu();
 });
 
 app.on('window-all-closed', () => {
